9897ce6b9ed4e081d4faeb82eafbbe1fb58a857e
Fawaaz Daniels <fawaazxdaniels@gmail.com>
2026-09-17 17:09:04 +0200

    chore: add src/app/sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,2 @@
+import { MetadataRoute } from 'next'; import { siteConfig } from '@/config/site'; export default function sitemap(): MetadataRoute.Sitemap{return ['','websites','how-it-works','examples','contact'].map(path=>({url:`${siteConfig.siteUrl}/${path}`,lastModified:new Date()}))}
+
